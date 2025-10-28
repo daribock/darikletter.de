5e19ab764245b2ee49971588577b43656d5eb84e
Darius Kletter <darikletter@gmail.com>
2025-10-28 17:19:23 +0100

    feat: add profile picture

--- app/header.tsx
+++ app/header.tsx
@@ -1,10 +1,20 @@
 import { TextEffect } from '@/components/ui/text-effect';
 import Link from 'next/link';
+import Image from 'next/image';
 import { metaData } from '../lib/data';
 
 export function Header() {
   return (
-    <header className="mb-8 flex items-center justify-between">
+    <header className="mb-8 flex items-center space-x-4">
+      <Link href="/">
+        <Image
+          src="/darikletter.png"
+          width="200"
+          height="200"
+          alt="darikletter"
+          className="h-[54px] w-[54px] rounded-full "
+        />
+      </Link>
       <div>
         <Link href="/" className="font-medium text-black dark:text-white">
           {metaData.name}
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,14 @@
 'use client';
+
 import { motion } from 'motion/react';
 import Image from 'next/image';
-import { XIcon } from 'lucide-react';
 import { Spotlight } from '@/components/ui/spotlight';
 import { Magnetic } from '@/components/ui/magnetic';
-import {
-  MorphingDialog,
-  MorphingDialogTrigger,
-  MorphingDialogContent,
-  MorphingDialogClose,
-  MorphingDialogContainer,
-} from '@/components/ui/morphing-dialog';
 import {
   PROJECTS,
   WORK_EXPERIENCE,
   EMAIL,
-  SOCIAL_LINKS,
+  socialLinks,
   metaData,
 } from '../lib/data';
 import Link from 'next/link';
@@ -23,8 +16,8 @@ import Link from 'next/link';
 function getGradientForProject(projectId: string) {
   const gradients = [
     'bg-linear-to-r from-cyan-500 to-blue-500',
-    'bg-linear-to-t from-sky-500 to-indigo-500',
     'bg-linear-to-bl from-violet-500 to-fuchsia-500',
+    'bg-linear-to-t from-sky-500 to-indigo-500',
     'bg-linear-65 from-purple-500 to-pink-500',
   ];
 
@@ -49,56 +42,6 @@ const VARIANTS_SECTION = {
 const TRANSITION_SECTION = {
   duration: 0.3,
 };
-
-type ProjectVideoProps = {
-  src: string;
-};
-
-function ProjectVideo({ src }: ProjectVideoProps) {
-  return (
-    <MorphingDialog
-      transition={{
-        type: 'spring',
-        bounce: 0,
-        duration: 0.3,
-      }}
-    >
-      <MorphingDialogTrigger>
-        <video
-          src={src}
-          autoPlay
-          loop
-          muted
-          className="aspect-video w-full cursor-zoom-in rounded-xl"
-        />
-      </MorphingDialogTrigger>
-      <MorphingDialogContainer>
-        <MorphingDialogContent className="relative aspect-video rounded-2xl bg-zinc-50 p-1 ring-1 ring-zinc-200/50 ring-inset dark:bg-zinc-950 dark:ring-zinc-800/50">
-          <video
-            src={src}
-            autoPlay
-            loop
-            muted
-            className="aspect-video h-[50vh] w-full rounded-xl md:h-[70vh]"
-          />
-        </MorphingDialogContent>
-        <MorphingDialogClose
-          className="fixed top-6 right-6 h-fit w-fit rounded-full bg-white p-1"
-          variants={{
-            initial: { opacity: 0 },
-            animate: {
-              opacity: 1,
-              transition: { delay: 0.3, duration: 0.1 },
-            },
-            exit: { opacity: 0, transition: { duration: 0 } },
-          }}
-        >
-          <XIcon className="h-5 w-5 text-zinc-500" />
-        </MorphingDialogClose>
-      </MorphingDialogContainer>
-    </MorphingDialog>
-  );
-}
 
 function MagneticSocialLink({
   children,
@@ -146,7 +89,7 @@ export default function Personal() {
         variants={VARIANTS_SECTION}
         transition={TRANSITION_SECTION}
       >
-        <div className="flex-1">
+        <div className="flex flex-col space-y-4">
           <p className="text-zinc-600 dark:text-zinc-400">
             {metaData.description}
           </p>
@@ -277,7 +220,7 @@ export default function Personal() {
           </a>
         </p>
         <div className="flex items-center justify-start space-x-3">
-          {SOCIAL_LINKS.map((link) => (
+          {Object.values(socialLinks).map((link) => (
             <MagneticSocialLink key={link.label} link={link.link}>
               {link.label}
             </MagneticSocialLink>
